429fa5975ac5a4e9dce9c6ae087f211f2f26f441
Matt Schulman <mattschulman@me.com>
2024-01-02 11:16:29 -0500

    Day 69 Part 2

--- Day_69_Blog_Capstone_Part_4/blog-with-users/forms.py
+++ Day_69_Blog_Capstone_Part_4/blog-with-users/forms.py
@@ -22,7 +22,11 @@ class RegisterForm(FlaskForm):
     submit = SubmitField("Sign Me Up!")
 
 
-# TODO: Create a LoginForm to login existing users
+# Create a LoginForm to login existing users
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log In")
 
 
 # TODO: Create a CommentForm so users can leave comments below posts
